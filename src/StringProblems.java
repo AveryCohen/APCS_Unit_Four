4af44f169adce0aa531ca83382dcb28afb0cdc6c
AveryCohen <AveryCohen04@gmail.com>
2021-11-10 09:54:25 -0500

    Finished 3 problems

--- src/StringProblems.java
+++ src/StringProblems.java
@@ -1,13 +1,23 @@
+import java.util.Locale;
+
 public class StringProblems {
 
     public static String addHyphens(String s) {
+        String newString = "";
+        for(int i=0; i<s.length(); i++) {
+            newString += s.substring(i, i+1) + "-";
+        }
 
-        return "";
+        return newString.substring(0, newString.length()-1);
     }
 
     public static String everyOtherLetter(String s) {
+        String newString = "";
+        for(int i=0; i<s.length(); i+=2) {
+            newString += s.substring(i, i+1);
+        }
 
-        return "";
+        return newString;
     }
 
 
@@ -17,6 +27,26 @@ public class StringProblems {
     }
 
     public static boolean isPalindrome(String word) {
-        return true;
+        String wordReversed = "";
+        String ithLetter, jthLetter;
+        String wordlow = word.toLowerCase();
+        String wordlownospace = "";
+        for(int i=0; i < wordlow.length(); i++) {
+            jthLetter = wordlow.substring(i,i+1);
+            if (!wordlow.substring(i, i+1).equals(" ")) {
+                wordlownospace += jthLetter;
+            }
+        }
+        for(int i=0; i < wordlownospace.length(); i++) {
+            ithLetter = wordlownospace.substring(i,i+1);
+
+            wordReversed = ithLetter + wordReversed;
+        }
+        if (wordReversed.toLowerCase().equals(wordlownospace)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
