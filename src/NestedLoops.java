a5630fd1e25a2de9fcd654f62a3347017ce3c08f
AveryCohen <AveryCohen04@gmail.com>
2021-11-11 10:56:11 -0500

    Finished 3 problems

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,17 +1,67 @@
 public class NestedLoops {
 
     public static String xSquare(int n) {
-
-        return "";
+        String square = "";
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= n; col++)
+            {
+                square+= "X";
+            }
+            square += "\n";
+    }
+        return square;
     }
 
     public static String flippedTriangle(int n) {
-
-        return "";
+        String triangle = "";
+        for (int row = n; row >= 1; row--)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                triangle+= "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
-    public static String fizzBuzz(int n) {
 
-        return "";
+    public static String fizzBuzz(int n) {
+        String fizz = "";
+        for (int row = 1; row<=n; row++)
+        {
+            if (row%20 == 1 && row !=1) {
+                fizz+= "\n";
+                if (row % 3 == 0 && row % 5 == 0) {
+                    fizz += "FizzBuzz ";
+                }
+                else if (row%5 ==0) {
+                    fizz+= "Buzz ";
+                }
+                else if (row%3 == 0) {
+                    fizz += "Fizz ";
+                }
+                else {
+                    fizz += row + " ";
+                }
+            }
+            else {
+                if (row % 3 == 0 && row % 5 == 0) {
+                    fizz += "FizzBuzz ";
+                }
+                else if (row%5 ==0) {
+                    fizz+= "Buzz ";
+                }
+                else if (row%3 == 0) {
+                    fizz += "Fizz ";
+                }
+                else {
+                    fizz += row + " ";
+                }
+            }
+        }
+        return fizz;
+
     }
 }
