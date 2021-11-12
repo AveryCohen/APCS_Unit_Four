3cc2383dc2f9ced946adf105e249e7c21294c100
AveryCohen <AveryCohen04@gmail.com>
2021-11-12 11:51:52 -0500

    Worked a bit on flipped triangle

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -46,6 +46,7 @@ public class NestedLoops {
                     fizz += row + " ";
                 }
             }
+
             else {
                 if (row % 3 == 0 && row % 5 == 0) {
                     fizz += "FizzBuzz ";
@@ -64,4 +65,29 @@ public class NestedLoops {
         return fizz;
 
     }
+    public static String sideways() {
+        int n =5;
+        int m = n/2;
+        int o = 0;
+        if (n%2 == 1) {
+            o+= m+1;
+        }
+        String triangle = "";
+        for (int p = 1; p >=5; p ++) {
+            for (int roww = 1; roww > o; roww++) {
+                for (int coll = 1; coll <= roww; coll++) {
+                    triangle += "*";
+                }
+                triangle += "\n";
+            }
+            for (int row = m; row >= 1; row--) {
+                for (int col = 1; col <= row; col++) {
+                    triangle += "*";
+                }
+                triangle += "\n";
+            }
+        }
+        return triangle;
+    }
 }
+
